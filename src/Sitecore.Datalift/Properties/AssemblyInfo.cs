b9daa2c7417ae61865a434ebf101b4795da8566c
Mark Cassidy <markc@corepoint-it.net>
2016-06-22 11:33:43 +0200

    Excluded Web.config from NuGet package

--- src/Sitecore.Datalift/Properties/AssemblyInfo.cs
+++ src/Sitecore.Datalift/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
-[assembly: AssemblyInformationalVersion("0.5.1.beta.RC2")]
+[assembly: AssemblyVersion("0.5.2.0")]
+[assembly: AssemblyFileVersion("0.5.2.0")]
+[assembly: AssemblyInformationalVersion("0.5.2.beta.RC2")]
