5a74215f79d8f9e6842b8bcbc918ba002de792ed
Mark Cassidy <mark@cassidy.dk>
2016-10-08 02:05:04 +0200

    Rewrote GetActionItem method on base DataliftController; as the parameter overload with optional parameters was a right mess. Also got rid of the testing project - it has to be rewritten from ground up; my hacky implementation of Database overrides is not compatible with the new changes for Database in Sitecore 8.2+

--- src/Sitecore.Datalift/DataliftController.cs
+++ src/Sitecore.Datalift/DataliftController.cs
@@ -1,53 +1,70 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Sitecore.Data.Items;
 using Sitecore.Datalift.Strategies;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Presentation;
 
 namespace Sitecore.Datalift
 {
-    public class DataliftController : Controller
-    {
-        protected virtual IDataliftAttribute GetStrategyAttribute()
-        {
-            var customAttributes = GetType().GetCustomAttributes(typeof (IDataliftAttribute), false);
-            if (customAttributes.Length > 0)
-            {
-                return customAttributes[0] as IDataliftAttribute;
-            }
+	public class DataliftController : Controller
+	{
+		protected virtual IDataliftAttribute GetStrategyAttribute()
+		{
+			var customAttributes = GetType().GetCustomAttributes(typeof(IDataliftAttribute), false);
+			if (customAttributes.Length > 0)
+				return customAttributes[0] as IDataliftAttribute;
 
-            return null;
-        }
+			return null;
+		}
 
-        protected virtual IDataliftStrategy GetDefaultStrategy()
-        {
-            return new DatasourceOrSelfStrategy();
-        }
+		protected virtual IDataliftStrategy GetDefaultStrategy()
+		{
+			return new DatasourceOrSelfStrategy();
+		}
 
-        protected virtual Item GetContextItem()
-        {
-            return Context.Item;
-        }
+		protected virtual Item GetContextItem()
+		{
+			return Context.Item;
+		}
 
-        protected virtual Item GetActionItem(string datasourceString = null, string templateIdentifier = null, Item contextItem = null, IDataliftStrategy strategy = null)
-        {
-            var att = GetStrategyAttribute();
+		protected virtual string GetDatasource()
+		{
+			return RenderingContext.CurrentOrNull?.Rendering.DataSource;
+		}
 
-            if (strategy == null && att != null)
-            {
-                strategy = att.Strategy;
-            }
+		/// <summary>
+		///     Looks at the current class attributes for a Strategy attribute and uses it to resolve the ActionItem. If no
+		///     attribute
+		///     is found, <see cref="GetDefaultStrategy" /> is used to resolve. If the attribute defines a TemplateIdentifier, it
+		///     is used to validate the ActionItem.
+		/// </summary>
+		/// <returns>
+		///     ActionItem or
+		///     <value>null</value>
+		/// </returns>
+		protected virtual Item GetActionItem()
+		{
+			var att = GetStrategyAttribute();
+			var strategy = att != null ? att.Strategy : GetDefaultStrategy();
+			return strategy.Resolve(GetDatasource(), GetContextItem(), att?.TemplateIdentifier);
+		}
 
-            if (templateIdentifier == null && att != null)
-            {
-                templateIdentifier = att.TemplateIdentifier;
-            }
+		/// <summary>
+		///     Looks at the current class attributes for a Strategy attribute and uses it to resolve the ActionItem. If no
+		///     attribute
+		///     is found, <see cref="GetDefaultStrategy" /> is used to resolve. <paramref name="templateIdentifier" /> overrides
+		///     any definition on the attribute.
+		/// </summary>
+		/// <param name="templateIdentifier"></param>
+		/// <returns></returns>
+		protected virtual Item GetActionItem(string templateIdentifier)
+		{
+			Assert.IsNotNull(templateIdentifier, nameof(templateIdentifier));
 
-            if (strategy == null)
-                strategy = GetDefaultStrategy();
-
-            if (contextItem == null)
-                contextItem = GetContextItem();
-
-            return strategy.Resolve(datasourceString, contextItem, templateIdentifier);
-        }
-    }
+			var att = GetStrategyAttribute();
+			var strategy = att != null ? att.Strategy : GetDefaultStrategy();
+			return strategy.Resolve(GetDatasource(), GetContextItem(), templateIdentifier);
+		}
+	}
 }
